f06e63bc74f419b69b6e89ab66f944e541158b02
Nabila Zahra <nabilazehra@hotmail.com>
2021-04-23 19:14:23 +0500

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import TodoListItem from "./Todo";
+import "./stylesheet.css";
+import firebase from "firebase";
+import db from "./firebase";
+import { Button } from "@material-ui/core";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  function getTodos() {
+    db.collection("todos").onSnapshot(function (querySnapshot) {
+      setTodos(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          todo: doc.data().todo,
+          inprogress: doc.data().inprogress,
+        }))
+      );
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    db.collection("todos").add({
+      inprogress: true,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      todo: input,
+    });
+
+    setInput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">T O D O</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="todo"
+          name="text"
+          placeholder="Create a new todo..."
+          autoComplete="off"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+        <button className="addtodo" type="submit">
+          ADD TODO
+        </button>
+      </form>
+      <div className="bg">
+        {todos.map((todo) => (
+          <TodoListItem
+            todo={todo.todo}
+            inprogress={todo.inprogress}
+            id={todo.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
